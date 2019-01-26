810887858a9b9929fe22ae2f649733d9eca2c75a
samuelhegner <C16439262@mydit.ie>
2019-01-26 17:19:30 +0000

    Camera Finished

---
+++ Assets/LookAtPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+
+    public bool test;
+
+    public GameObject testLookAt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(test)
+        transform.LookAt(testLookAt.transform.position);
+    }
+}
