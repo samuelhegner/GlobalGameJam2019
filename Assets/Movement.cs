e4b1957402daba6f242686a906f4b57c3e14c33c
samuelhegner <C16439262@mydit.ie>
2019-01-26 14:19:59 +0000

    Movement and Tag

---
+++ Assets/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    Camera cam;
+
+    bool activePlayer;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        cam = Camera.main;
+    }
+
+    void Start()
+    {
+        agent.updateRotation = false;
+    }
+
+    void Update()
+    {
+        //click to move
+        if (Input.GetMouseButton(1)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 1000f))
+            {
+                
+                if (hit.transform.tag == "Ground")
+                {
+                    print("test" + hit.point);
+                    agent.SetDestination(hit.point);
+                    Debug.DrawRay(ray.origin, ray.direction * 1000f, Color.red);
+                }
+            }
+
+        }
+
+    }
+
+    
+}
