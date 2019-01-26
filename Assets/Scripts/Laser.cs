96fa78935848db80d23ab6a3262ca0f2c7dacdbc
PancreasMonster <ryanflood444@gmail.com>
2019-01-26 14:10:32 +0000

    LaserSplit

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -6,8 +6,10 @@ public class Laser : MonoBehaviour
 {
     public LayerMask layerMask;
     LineRenderer LR;
-    public LineRenderer LR2, LR3;
+    //public LineRenderer LR2, LR3;
     Transform holder;
+    public List<GameObject> objects = new List<GameObject>();
+    public GameObject currentObject;
 
 
     // Start is called before the first frame update
@@ -34,27 +36,32 @@ public class Laser : MonoBehaviour
            // Debug.Log(hit.transform.tag);
             if (hit.transform.tag == "yes")
             {
-                LR2.SetPosition(0, hit.transform.position);
+               /* LR2.SetPosition(0, hit.transform.position);
                 LR3.SetPosition(0, hit.transform.position);
                 LR2.SetPosition(1, GameObject.Find(hit.transform.name.ToString() + " 1").transform.position);
                 LR3.SetPosition(1, GameObject.Find(hit.transform.name.ToString() + " 2").transform.position);
-               // hit.transform.GetComponent<LaserHit>().HitByLaser();
+                RaycastHit hit2;*/
+                hit.transform.GetComponent<LaserHit>().HitByLaser();
+                currentObject = hit.transform.gameObject;
             }
             else
             {
-                LR2.SetPosition(0, holder.position);
-                LR3.SetPosition(0, holder.position);
-                LR2.SetPosition(1, holder.position);
-                LR3.SetPosition(1, holder.position);
+                if (currentObject != null)
+                    StartCoroutine(stopSplitter());
             }
-            {
-
-            }
+           
         }
         else
         {
            // Debug.DrawLine(transform.position, transform.TransformDirection(Vector3.right) * hit.distance, Color.yellow);
             //Debug.Log("Did not Hit");
         }
+    }
+
+    IEnumerator stopSplitter()
+    {
+        currentObject.GetComponent<LaserHit>().stopLaser();
+        yield return new WaitForSeconds(Time.deltaTime);
+        currentObject = null;
     }
 }
