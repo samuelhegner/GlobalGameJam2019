333b208b89f966eab742e010c3543e4847b7516b
gazor117 <oreillygary7@gmail.com>
2019-01-27 01:09:23 +0000

    Particle speed change

--- Assets/ParticleMovement.cs
+++ Assets/ParticleMovement.cs
@@ -9,6 +9,9 @@ public class ParticleMovement : MonoBehaviour
     public static Vector3 targetPos;
     public static bool enableParticleSystem = false;
     public float speed = 10f;
+
+    public float minSpeed, maxSpeed;
+    public float maxDistance;
     
     
     
@@ -21,21 +24,48 @@ public class ParticleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float setSpeed = SetMoveSpeed(minSpeed, maxSpeed,targetPos, transform.position );
         if (enableParticleSystem)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-            print("working");
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, setSpeed* Time.deltaTime);
+       
 
             if (transform.position == targetPos)
             {
-                Invoke("DisableObject", 1f);
+                Invoke("DisableObject", 0.2f);
             }
         }
+
+        print(setSpeed);
+
+
     }
 
 
     void DisableObject()
     {
         gameObject.SetActive(false);
+    }
+    
+    public float SetMoveSpeed(float min, float max, Vector2 target, Vector2 current)
+    {
+        float dist = Vector2.Distance(target, current);            //Distance between target and pos of this object
+        float targetSpeed;
+        if (dist < maxDistance)                                    //if that distance is less than max distance
+        {
+            float speedPercent = dist / maxDistance;                //gets % of current dist relative to max distance
+            targetSpeed = maxSpeed * speedPercent;                    // multiplies max speed by this % getting target speed
+        }
+        else
+        {
+            targetSpeed = max;
+        }
+
+        if (targetSpeed < min)
+        {
+            targetSpeed = min;
+        }
+
+        return targetSpeed;
     }
 }
