b807dcfd4664b104719174ee37675b5666fc6c19
samuelhegner <C16439262@mydit.ie>
2019-01-27 13:10:54 +0000

    Png added

--- Assets/Menu_Manager.cs
+++ Assets/Menu_Manager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Menu_Manager : MonoBehaviour
 {
+
+    enum Menu { Default, HowTo };
+
+    public GameObject[] menus = new GameObject[2];
+
+    [SerializeField]
+    Menu currentState;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,25 @@ public class Menu_Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentState == Menu.Default)
+        {
+            menus[0].SetActive(true);
+            menus[1].SetActive(false);
+        }
+        else if (currentState == Menu.HowTo) {
+            menus[0].SetActive(false);
+            menus[1].SetActive(true);
+        }
+    }
+
+    void SwitchMode()
+    {
+        if (currentState == Menu.Default)
+        {
+            currentState = Menu.HowTo;
+        }
+        else if (currentState == Menu.HowTo) {
+            currentState = Menu.Default;
+        }
     }
 }
